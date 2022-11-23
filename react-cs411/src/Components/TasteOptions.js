514cac31c71c99840fbdbb4ddb99c5d3fd1aedf5
aonnadi <aninja1008@gmail.com>
2022-11-23 15:35:59 -0500

    Add the taste options to the home page

---
+++ react-cs411/src/Components/TasteOptions.js
@@ -0,0 +1,17 @@
+
+import React from "react"
+import "../css/TasteOptions.css"
+
+const TasteOptions = () => {
+    const tastes = ["Sweet", "Sour", "Salty", "Savory", "Bitter"]
+    return(
+        <div className="container">
+            <div className="options-bar">
+                <div className="options">
+                    Sweet Sour Salty Savory Bitter
+                </div>
+            </div>
+        </div>
+    )
+}
+export default TasteOptions
