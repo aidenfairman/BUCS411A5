c89c109db81057de648c5a2949f57501fe74ea3e
aonnadi <aninja1008@gmail.com>
2022-12-08 11:55:32 -0500

    Create login page component and add more app instructions for the users

--- react-cs411/src/Components/HowToUse.js
+++ react-cs411/src/Components/HowToUse.js
@@ -7,10 +7,16 @@ const HowToUse = () => {
         <div>
             <Card sx={{width:"800px", marginLeft:"360px"}}>
                 <Typography>How to use website:</Typography> 
-                <Typography> Feature 1: </Typography>
-                <Typography> Search up to 3 tastes. Separate each taste with a space.</Typography>
+                <Typography> Option 1: </Typography>
+                <Typography> Select up to 3 tastes to search for. Separate each taste with a space.</Typography>
                 <Typography> Ex. You can search for <b>sweet</b> recipes by entering "Sweet"</Typography>
                 <Typography> Ex. You can search for <b>sweet</b> AND <b>sour</b> recipes by entering "Sweet Sour"</Typography>
+
+                <br></br>
+
+                <Typography>Option 2: Nutrition </Typography>
+                <Typography>Check a Diet Option and the search will only return recipes that satisfies your nutrition goals</Typography>
+                <Typography> Ex. <b>Search: "sweet" </b> and <b> Select: "Stay in Shape" </b> to search sweet recipes that allow you to stay in shape.</Typography>
 
             </Card>
         </div>
---
+++ react-cs411/src/Components/Login.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+
+function FavButton() {
+    return (
+      <div className="center">
+        hi
+      </div>
+    );
+  }
+  
+  export default FavButton;
