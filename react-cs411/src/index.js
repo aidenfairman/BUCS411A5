06cd84e4a41740107f54350c0a4af7f28d179755
Connor_Windows <shuyangconnor@gmail.com>
2022-12-09 03:31:46 -0500

    Merge

--- react-cs411/src/index.js
+++ react-cs411/src/index.js
@@ -7,12 +7,8 @@ import { GoogleOAuthProvider } from '@react-oauth/google'
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
-<<<<<<< HEAD
-    <GoogleOAuthProvider clientId="316363094058-0lea28iimm7noqssrh3as176r3vj69pd.apps.googleusercontent.com"><App /></GoogleOAuthProvider>;
-=======
     <BrowserRouter>
-    <App />
+      <App />
     </BrowserRouter>
->>>>>>> 132714df1cb9c153bc03b9dd525ddc6622cd51af
   </React.StrictMode>
 )
