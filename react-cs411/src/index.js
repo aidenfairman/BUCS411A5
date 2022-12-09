e5917b69eaa7d03798bff02eb13f19e799bc7414
Connor_Windows <shuyangconnor@gmail.com>
2022-12-09 02:46:32 -0500

    OAuth Login

--- react-cs411/src/index.js
+++ react-cs411/src/index.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import { GoogleOAuthProvider } from '@react-oauth/google'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
-    <App />
+    <GoogleOAuthProvider clientId="316363094058-0lea28iimm7noqssrh3as176r3vj69pd.apps.googleusercontent.com"><App /></GoogleOAuthProvider>;
   </React.StrictMode>
 )
